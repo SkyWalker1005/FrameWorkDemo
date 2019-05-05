48db2040f8fe0a3523e4b3620cf88e4a71d43d68
Home <Home@NarenderSharma>
2019-05-05 14:49:09 +0530

    Added webdrivers as resources

--- FrameWorkDemo/src/framework/BaseTest.java
+++ FrameWorkDemo/src/framework/BaseTest.java
@@ -20,25 +20,25 @@ public class BaseTest {
   @Parameters({"browserName","webSite"})
   public void setup(String browserName,String webSite) throws Exception {
 	  if(browserName.equalsIgnoreCase("chrome")){
-			System.setProperty("webdriver.chrome.driver", "D:\\Lalit\\Selenium\\drivers\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\resources\\chromedriver.exe");
 			driver=new ChromeDriver();
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			}
 			else if(browserName.equalsIgnoreCase("firefox")){
-				System.setProperty("webdriver.gecko.driver", "D:\\Lalit\\Selenium\\drivers\\geckodriver.exe");
+				System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\src\\resources\\geckodriver.exe");
 				driver=new FirefoxDriver();
 				driver.manage().window().maximize();
 				driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 				}
 			else if(browserName.equalsIgnoreCase("ie")){
-				System.setProperty("webdriver.ie.driver", "D:\\Lalit\\Selenium\\drivers\\IEDriverServer.exe");
+				System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"\\src\\resources\\IEDriverServer.exe");
 				driver=new InternetExplorerDriver();
 				driver.manage().window().maximize();
 				driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 				}
 			else if(browserName.equalsIgnoreCase("edge")){
-				System.setProperty("webdriver.edge.driver", "D:\\Lalit\\Selenium\\drivers\\MicrosoftWebDriver.exe");
+				System.setProperty("webdriver.edge.driver", System.getProperty("user.dir")+"\\src\\resources\\MicrosoftWebDriver.exe");
 				driver=new EdgeDriver();
 				driver.manage().window().maximize();
 				driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
